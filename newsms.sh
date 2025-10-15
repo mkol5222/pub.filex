35b4d4820ec8313d979e2ed58ac499327a3f1f99
Martin Koldovsky <mkoldov@gmail.com>
2025-10-15 12:36:42 +0200

    updated bin

---
+++ newsms.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TS=$(date +%s)
+echo "[$TS] This is mockup send sms script"
+echo "Phone: $NEWSMS_PHONE"
+echo "SMSTEXT: $NEWSMS_SMSTEXT"
+
+# run curl_cli to real provider here and use variables $NEWSMS_PHONE and $NEWSMS_SMSTEXT
+
+echo "SMS sent (mockup)"
+echo ""
+
+exit 0
