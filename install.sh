ea47ef53ac6ef78b5f3497691e035d4aeb4100f8
Martin Koldovsky <mkoldov@gmail.com>
2025-10-16 19:18:57 +0200

    updated bin

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ export PATH
 
 NAME="NEWSMS"
 BIN_PATH="/opt/newsms/newsms"
-CMD="/bin/bash -c '/opt/newsms/newsms | tee -a /var/log/newsms.log'"
+CMD="/usr/bin/sh -c 'cd /opt/newsms && ./newsms | tee -a /var/log/newsms.log'"
 
 start() {
     echo "Starting $NAME..."
@@ -119,6 +119,27 @@ cat << 'EOF' > $LOGROTATE_CONF
 }
 EOF
 
+cat << 'EOF' > $BASE_FOLDER/newsms.sh
+#!/bin/bash
+
+TS=$(date +%s)
+echo "[$TS] This is mockup send sms script"
+echo "Phone: $NEWSMS_PHONE"
+echo "SMSTEXT: $NEWSMS_SMSTEXT"
+
+# run curl_cli to real provider here and use variables $NEWSMS_PHONE and $NEWSMS_SMSTEXT
+
+echo "SMS sent (mockup)"
+
+curl -s -d '{"phone":"'"$NEWSMS_PHONE"'","message":"'"$NEWSMS_SMSTEXT"'"}' \
+https://nanuc-1.buru-gamma.ts.net/webhook/3e462382-59dc-4a38-9b92-1776c441dc45  \
+-u sms:vpn123 -H "Content-Type: application/json"
+echo ""
+
+exit 0
+EOF
+
+chmod +x $BASE_FOLDER/newsms.sh
 
 chmod +x $INIT_SCRIPT
 
